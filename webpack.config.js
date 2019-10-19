ff793c399d3281ca00adfe272628938111976af9
Valeriy Zimnev <sorption.music@gmail.com>
2019-10-20 01:20:55 +0300

    Update building

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     node: {
         fs: 'empty',
     },
     target: 'node',
-    externals: [nodeExternals()],
+    // externals: [nodeExternals()],
     // entry: ['intersection-observer', './src/index.js'],
     entry: './index.js',
     output: {
@@ -22,8 +24,10 @@ module.exports = {
             },
         ],
     },
-    plugins: [
-    ],
+    plugins: process.env.NODE_ENV === 'production' ? [
+        new webpack.ProgressPlugin(),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+    ] : [],
     stats: {
         colors: true,
     },

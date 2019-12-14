76cb94e3977b3bf45976bdc055f1ac4488b04ffb
Valeriy Zimnev <sorption.music@gmail.com>
2019-12-14 15:18:57 +0300

    Fix CleanWebpackPlugin

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
